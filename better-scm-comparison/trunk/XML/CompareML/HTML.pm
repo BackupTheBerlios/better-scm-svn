57a1946c309dd1667faa67c400b0fc20c96da6f0
shlomif <shlomif@38b9c29c-9acb-40d3-b369-b575a8dbbeea>
2004-08-28 10:51:12 +0000

    Fixed the encoding in the resultant HTML file.

--- better-scm-comparison/trunk/XML/CompareML/HTML.pm
+++ better-scm-comparison/trunk/XML/CompareML/HTML.pm
@@ -61,7 +61,7 @@ EOF
 <!DOCTYPE html
      PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="he" lang="he">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 <title>Version Control Systems Comparison</title>
